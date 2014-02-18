06f5c02becfe30557a08a7d8d51584a65ecdbc24
John Jacobs <john@linux-cvax.site>
2014-02-18 02:10:42 +0100

    Fixed SwiftFile spec

--- lib/swift.rb
+++ lib/swift.rb
@@ -1,5 +1,4 @@
-require_relative 'swift_classes/swift_line'                        # base class
-require_relative 'swift_classes/swift_message'                     # swift message
+require_relative 'swift_classes/swift_line'                        # swift line base class
 require_relative 'swift_classes/swift_message_header'              # message header
 require_relative 'swift_classes/swift_transaction_reference'       # tag :20:
 require_relative 'swift_classes/swift_related_reference'           # tag :21:
@@ -12,3 +11,6 @@ require_relative 'swift_classes/swift_available_balance'           # tag :64:
 require_relative 'swift_classes/swift_forward_balance'             # tag :65:
 require_relative 'swift_classes/swift_message_description'         # tag :86:
 require_relative 'swift_classes/swift_message_trailer'             # message trailer
+
+require_relative 'swift_classes/swift_message'                     # swift message
+require_relative 'swift_classes/swift_file'                        # swift file
--- spec/swift_file_spec.rb
+++ spec/swift_file_spec.rb
@@ -1,15 +1,8 @@
 require 'spec_helper'
 require 'swift'
-require 'swift_converter'
 
-describe SwiftConverter do
+describe SwiftClasses::SwiftFile do
   it 'raises an RuntimeError if not initialized with a File object' do
-    expect { SwiftConverter.new( 'invalid file object' ) }.to raise_error( RuntimeError )
-  end
-
-  it 'returns a non empty array when given a valid SWIFT file' do
-    swift_file = SwiftConverter.new( File.new( 'data/DFEZ.SWIFT', 'r') )
-    swift_array = swift_file.swift_array
-    expect( swift_array.empty? ).to be_false
+    expect { SwiftClasses::SwiftFile.new( 'invalid file object' ) }.to raise_error( RuntimeError )
   end
 end
